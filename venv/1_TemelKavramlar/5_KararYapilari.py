e6bfe6af30f9059e1717a0f9eab1d0984dabb38c
gonca.comak <gonca.0045@hotmail.com>
2022-10-16 13:39:56 +0300

    KARAR YAPILARI

--- venv/1_TemelKavramlar/5_KararYapilari.py
+++ venv/1_TemelKavramlar/5_KararYapilari.py
@@ -236,11 +236,152 @@ bunlar dışında bir giriş yapılırsa "ürün bulunmamaktadır" uyarısı ver
 
 
 
+# rastgele sayı üretmek için :
+# import random #dışarıdan bir dosyayı kullanmak için kullanırız.
+# r1=random.randint(1,10)
+#
+# import random as rnd
+#
+# r2=rnd.randint(1,20)
+# print(r1,r2)
+
+# 1-10 arasında rastgele üretilen 3 adet sayının hangisinin büyük olduğuna bulan ve ekranda hem sayıları hem en büyük sayı:{x} gibi yazdıran uygulamayı yazınız.
+
+# import random
+# r1=random.randint(1,10)
+# r2=random.randint(1,10)
+# r3=random.randint(1,10)
+# if (r1>r2 and r1>r3):
+#     print(f"En büyük sayı:{r1}'dir,{r1},{r2},{r3}")
+# elif (r2>r1 and r2>r3):
+#     print(f"En büyük sayı:{r2}'dir.{r1},{r2},{r3}")
+# else:
+#     print(f"En büyük sayı:{r3},dir.{r1},{r2},{r3}")
+#
+# #Sorunun farklı bir çözümü :
+# import random
+# r1=random.randint(1,10)
+# r2=random.randint(1,10)
+# r3=random.randint(1,10)
+# en_buyuk_sayi=0
+# if (r1>=r2 and r1>=r3):
+#     en_buyuk_sayi=r1
+# elif (r2>=r1 and r2>=r3):
+#     en_buyuk_sayi=r2
+# else:
+#     en_buyuk_sayi=r3
+# print(f"r1={r1}\n r2={r2}\n r3={r3} en büyük olan={en_buyuk_sayi}")
+#
+# #metot ile yapılan çözümü:
+# en_buyuk_sayi=max(r1,r2,r3) #istenilen sayıda parametre alabilir.
+# print(en_buyuk_sayi)
+
+# girilen boy ve kilo bilgileri ile beden kitle indekxi hesaplayan bir uygulama yazınız:
+# BKI=Kilo/(boy*2)  kilo(kg) boy(m)
+'''
+# • Beden kitle/kütle indeksi < 18,5 ise Zayıf
+# •    18,5 < Beden kitle/kütle indeksi < 24,9 ise Normal
+# •    25 < Beden kitle/kütle indeksi < 29,9 ise Fazla kilolu
+# •    30 < Beden kitle/kütle indeksi < 34,9 ise I. derece obez
+# •    35 < Beden kitle/kütle indeksi < 39,9 ise II. derece obez
+# •    Beden kitle/kütle indeksi > 40 ise III. derece obez
+
+'''
+
+
+#
+# kilo=float(input("Kilonuzu giriniz(kg):"))
+# boy=float(input("Boyunuzu giriniz(m):"))
+# endeks=(kilo/(boy **2),2)
+# durum=""
+# if endeks < 18.5 :
+#     durum="zayıf"
+# elif 18.5 <= endeks < 24.9:
+#     durum="normal"
+# elif 25 <= endeks < 29.9:
+#     durum="fazla kilolu"
+# elif 30 <= endeks < 34.9:
+#     durum="I.derece obez."
+# elif 35 <= endeks < 39.9:
+#     durum="II.derece obez"
+# elif endeks >=40:
+#     durum="III.derece obez"
+# else:
+#     durum="hesaplanamadı"
+#
+# print(f"Boy:{boy*100}cm Kilo:{kilo} kg Endeks:{endeks} Durum:{durum}")
 
 
 
+# Match-case: 3.10 sürümünde geldi.switch case yapısının karşılığıdır.
+'''
+match karsılastırma_yapilacak_degisken:
+    case karsilastirilacak_deger1:
+        islemler
+    case karsilastirilacak_deger2:
+        islemler
+    case _:
+        hicbirine uymazsa burası çalışır
+'''
+
+# kullanıcıdan haftanın her gününü alıp o günün sayısal karşılığını verelim (pazartesi: haftanın 1.günü)
+# gun=input("Gün giriniz:")
+# if gun=="Pazartesi"
+#     print("Haftanın 1.günü")
+# elif gun=="salı"
+#     print("Haftanın 2.günü")
+# # ...
+# # else:
+# #    print("BÖYLE BİR GÜN BULUNAMADI ,LÜTFEN KONTOL EDİNİZ.)
+
+# gun=input("Gün giriniz:")
+# match gun:
+#     case "pazartesi":
+#         print("Haftanın 1.günü")
+#     case "salı":
+#         print("Haftanın 2.günü")
+#     case "çarşamba":
+#         print("Haftanın 3.günü")
+#     # ...
+#     case _:
+#         print("Böyle bir gün bulunamadı.lütfen kontrol ediniz")
 
 
+# or ile match kullanımı (pipe işareti -alt gr + z'nin solundaki tuş)
+# kllanıcıdan alamk istediği ürünü isteyerek ürünün hangi reyonda olduğunu gösteren bir uygulama yapınız.
+'''
+# Domates,biber,patlıcan ->Sebze reyonu
+# Parfüm,diş macunu,şampuan ->Kozmetik
+# Cep Telefonu,bilgisayar,ses sistemleri ->Teknoloji reyonu
+# bunlar dışında bir giriş yapılırsa "ürün bulunmamaktadır" uyarısı vericektir.
+# '''
+# urun=input("Aradığınız ürünü giriniz:").lower()
+# match urun:
+#     case"domates"| "biber"| "patlıcan":
+#         print("sebze reyonu")
+#     case"parfüm"| "şampuan" |"diş macunu":
+#         print("Kozmetik reyonu")
+#     case _:
+#         print("ürün bulunmamaktadır.")
 
 
+# and ile match örneği:
+# kullanıcıdan kullanıcı ve şifre istedikten sonra ka:admin ve şifre:1234 ise giriş başarılı değilse,hangi bilgi hatalıysa onun uyarısını veren bir uygulama yazınız.
+
+kullanici_adi=input("Kullanıcı adınızı giriniz:")
+sifre=input("Kullanıcı şifrenizi giriniz:")
+match kullanici_adi:
+    case "admin":
+        match sifre:
+            case"1234":
+                print("Giriş başarılıdır")
+
+            case _:
+               print("şifreniz yanlıştır")
+
+    case _:
+        print("Kullancı adınız yanlış")
+
+# if 1==1:
+#    pass
 
---
+++ venv/1_TemelKavramlar/6_HataYonetimi.py
@@ -0,0 +1,79 @@
+#Hata yönetimi (exception handling)
+'''
+HATA TİPLERİ:
+1.Syntax (sözdizimi) Hataları:
+Python'da kod yazarrken uyulması gereken syntax'da uyulmadığında oluşan hatalardır.
+Programı çalıştırdığınızda console'da satır numarası ve hata mesajıyla birlikte hata görünür ancak program çalışmaz.
+'''
+# var sayi=10  ->syntax hatası  ->>var_sayi=10
+#   sayi=10  ---> boşluk hatası
+
+'''
+2.Çalışma zamanı hataları (runtime expection)
+syntax hatası yoktur,ancak uygulama çalışırken yönetilmemiş ve beklenmeyen hatalar oluşabilir.
+'''
+# sayi1=10
+# sayi2=input("sayi2 girin")
+# toplam=sayi1+sayi2  ##### (stringler string dışındaki veri tipi ile + işaretiyle toplanamaz.)
+
+'''
+try:
+    #hata almasını beklediğiniz kodlar bu blokta yazılır.
+except:(required)zorunlu
+    #yukarıdaki kodda bir hata oluşursa bu bloğa düşer.
+finally: (optional) opsiyonal
+    #hata oluşsa da oluşmasa da bu blok çalışır.
+
+'''
+# try:
+#     sayi1=10
+#     # sayi2==int(input("sayi2 giriniz")) bu düzgün çalışır.
+#     sayi2=input("sayi2 giriniz") #excepte düşer
+#     toplam=sayi1+sayi2
+#     print(toplam)
+# except:
+#     print("beklenmeyen bir hata oluştu.")
+# finally:
+#     print("yeniden deneyebilirsiniz")
+
+# typo:yazım denetimi hata değildir.
+
+# sınıflar ilki büyük harfle yazılır Exception bir sınıf örneğidir.Exception genel olarak bütün hataları kapsıyor:syntax olabilir,file hatası olabilir.
+try:
+    file=open("böylebirdosyayok.txt")
+except FileNotFoundError as fnex:
+    print(f"Bir hata oluştu.hata mesajı:{fnex.strerror}, hatalı dosya adı:{fnex.filename}")
+except FileExistsError as fex:
+    print(f"Bu dosyadan bir tane daha var.Mesaj:{fex.strerror} Hatalı dosya adı:  {fex.filename}")
+except Exception as ex :
+    print(f"Beklenmedik bir hata:{ex.args[1]}")
+
+'''
+3.MANTIKSAL HATALAR(LOGİCAL EXCEPTİONS)
+Developer hatasıdır,bulunması zordur.
+bki=kilo/(boy*2) --->burada karesi alınmak istenirken 2 ile çarpılmış bir mantıksal hata elde edilir.
+DEBUG yaparak adım adım kod incelenir ve hata bulunmaya çalışılır.
+DEBUG: program çalışırken adım adım kodu ilerletmek demektir.
+Debug yaparken:
+1.break point eklenir.(kodun sol tarafına line number'ın hemen sağına tıklayınız.)
+2.projeyi debug modda çalıştırın.(alt +f5)
+3.kod break pointe düştükten sonra f10 ile adım adım ilerleyebiliriz
+'''
+# boy=1.6
+# kilo=78
+# bki=kilo/(boy*2) #burada karesi alınmak istenirken 2 ile çarpılmış bir mantıksal hata elde edilir.
+# print(bki)
+
+
+##Hata fırlatma:  raise exception ("mesaj")
+try:
+    vize = float(input("vize notunu giriniz:"))
+    if not (vize >=0 or vize <=100):
+        raise Exception("0 ile 100 aralığında değer girilebilir.")
+    vize_agırlık=vize*0.4
+    print(vize_agırlık)
+except ValueError as ve:
+    print("Lütfen sayısal bir değer giriniz.")
+except Exception as ex:
+    print(ex)
+
