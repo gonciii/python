aac29bea27697752a008fe4986a61acfd9d91471
gonca.comak <gonca.0045@hotmail.com>
2022-10-22 13:51:19 +0300

    döngüler

---
+++ venv/1_TemelKavramlar/7_Donguler.py
@@ -0,0 +1,143 @@
+#DÖNGÜLER
+# ---------> 1.For
+# Belli bir collection içinde gezinerek,dönerek tekrarlı olarak yapılması istenen işlemleri gerçekleştirir.
+# liste tanımı:[1,2,3,4,5] ---->python yazımı
+# range(n):[0,1,2,.....,n)----->python yazımı değil matematiksel gösterimi.
+'''
+for iterasyon_name in collection:
+    tekrarlı olarak yapılacak işlemler
+
+# collection içinde okunacak veri kalmayana dek döner.
+'''
+# for isim in ["damla","gonca","aykut","arif","neslihan","tayfun"]:
+#     print(isim)
+#
+# # rakamlar listesi oluşturup ekranda alt alta yazınız.
+# for rakam in ["1","2","3","4","5","6","7","8","9"]:
+#     print(rakam)
+# for rakam in [1,2,3,4,5,6,7,8,9,0]:
+#     print(rakam**2)
+# rakamlar_listesi=[1,2,3,4,5,6,7,8,9,0]
+# for rakam in rakamlar_listesi:
+#     print(rakam**2)
+# range(10)--->[0,1,2,3,4,5,6,7,8,9]
+# for i in range(10):
+#     print(i)
+# 10 kez merhaba dünya yazdırınız.
+# for i in range(10,):
+#     print(f"{i+1}.merhaba dünya!")
+
+# 50-100 aralığındaki çift sayıları yazınız.
+# for i in range(50,101):
+#     if i % 2==0:
+#         print(i)
+#
+# # range(5,10)--->[5,6,7,8,9]
+# # range(10,20,2)--->[10,12,14,16,18]
+# for i in range(50,101,2):
+#     print(i)
+# # TEKLER İÇİN:
+# for i in range(51,101,2):
+#     print(i)
+
+# for örnekleri:
+# 10-20 arasındaki sayıları geriden yazdırınız.20-19-18-...11
+#for i in range (20,10,-1):
+#    print(i)
+
+# 0-20 arasındaki tek sayıların toplamını döngü ile bulunuz.
+# toplam=0
+# for i in range(1,20,2):
+#     toplam += i    #toplam=toplam+i
+# print(toplam)
+
+# 1-100 arasındaki teklerin  ve çiftlerin ayrı ayrı toplamını aynı döngüde hesaplayınız.
+# cift_toplam=0
+# tek_toplam=0
+# for i in range(1,101):
+#     if i % 2==0:  #çift
+#         cift_toplam +=i
+#     else:
+#         tek_toplam +=i
+#
+# # print(f"TEKLERİN TOPLAMI:{tek_toplam} \n ÇİFTLERİN TOPLAMI:{cift_toplam} ")
+# # sum(T): içindeki değerleri toplanarak sonuç döner.
+# print(f"teklerin toplamı:{sum(range(1,101,2))}")
+# print(f"çiftlerin toplamı: {sum(range(0,101,2))}")
+'''
+nested loop(iç içe döngüler)
+for i in collection1:
+    for j in collection2:
+        pass
+    pass
+    
+'''
+# çarpım tablosunu döngü kullanarak yazdırınız.
+# title_list = ["1'ler","2'ler","3'ler","4'ler","5'ler","6'lar","7'ler","8'ler","9'lar","10'lar"]
+# for i in range(1,11):
+#     print(title_list[i-1])
+#     print("_________")
+#     for j in range(1,11):
+#         print(f"{i}x{j}={i*j}")
+#     print("__________")
+# end: ne ile biteceğini belirliyor.\t bir tab demek.
+# yan yana çarpım tablosunu yazdırma.
+# title_list = ["1'ler","2'ler","3'ler","4'ler","5'ler","6'lar","7'ler","8'ler","9'lar","10'lar"]
+# for i in title_list:
+#     print(i,end="\t"*2)
+# for i in range(1,11):
+#     print("")
+#     for j in range(1,11):
+#         print(f"{i}x{j}={i * j}",end="\t")
+
+# # Döngü kırıcı ve ya atlatıcı :
+# 1.continue: döngüde continue görüldüğünde o anki iterasyon devam etmez sonraki iterasyona atlanır.
+
+# for i in range(10):
+#     if i ==7:
+#         continue
+#     print(i)
+#print kısmını en sona yazmak zorundayız.en başa yazdığımızda continue işe yaramamş olur.
+# 2.break: döngüde break görüldüğünde döngüden tamamen çıkılır."yani döngü tamamen kırılmış olur".
+# for i in range(10):
+#     if i==3:
+#         break
+#     print(i)
+# print("buradaki kodlar okunur.")
+
+# ----------> 2.While:
+# while döngüsünde koşul doğru(true) olduğu sürece döngü çalışmaya devam eder.
+'''
+while kosul:
+    #işlemler
+    
+**** koşulda sonsuz döngüye girecek yapıları yönetmemiz gerekir yoksa sonsuz döngüye gireriz.***  
+'''
+
+# while ile 1'den 10' a kadar sayıları yazdırın.
+# sayac=1
+# while sayac < 10:
+#     print(sayac)
+#     sayac +=1
+
+# Kullanıcı hayır yazana kadar merhaba dünya yazan uygulamayı yazınız.
+# devam_mi="evet"
+# while devam_mi=="evet":
+#     print("merhaba dünya")
+#     devam_mi=input("devam etmek istiyor musunuz? evet/hayır")
+
+# kullanıcı hayır diyene kadar rastgele sayı üreterek (1-100) ekranda gösteren uygulamayı yazınız.
+
+# 1.yöntem:
+import random as rnd
+while True:
+    print(f"Üretilen:{rnd.randint(1,100)}")
+    input("Çıkmak için hayır yazınız.")
+    if girilen =="hayır":
+        break
+
+# diğer yöntem
+devam_mi="e"
+while devam_mi=="e":
+    print(f"Üretilen :{rnd.randint(1,100)}")
+    devam_mi=input("devam etmek için e yazınız.")
