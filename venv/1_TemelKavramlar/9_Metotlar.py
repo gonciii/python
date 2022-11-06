3b5595d62f77b758b2cbf1f4432f19c7a4809819
gonciii <gonca.0045@hotmail.com>
2022-11-06 13:53:02 +0300

    Collection

---
+++ venv/1_TemelKavramlar/9_Metotlar.py
@@ -0,0 +1,10 @@
+#METOTLAR:
+#bir kez tanımlayıp birden fazla kez kullandığımız yapılardır.
+#1-)Hazır sistemdeki metotlar
+#string metotlar
+#matematiksel metotlar
+#datetime metotlar
+#2-)Custom yazılan metotlar
+#2 ye ayrılır:
+# 2.a-)Geri değer dönen metotlar
+# 2.b-)Geri değer dönmeyen metotlar
